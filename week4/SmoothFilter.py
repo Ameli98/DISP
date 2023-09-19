70b5a1ad768c7426a95c49f20a0a83a1657135b6
Ameli98 <rain0210303@gmail.com>
2023-09-19 12:16:49 +0800

    FEAT: SFilter & EDFilter w/ 2 kinds of filter

--- week4/SmoothFilter.py
+++ week4/SmoothFilter.py
@@ -2,46 +2,44 @@ import numpy as np
 import matplotlib.pyplot as plt
 from Filter import Filter
 
-
 if __name__ == "__main__":
-    TIME = np.arange(-30, 101, 1)
-    OFFSET1 = 30
-    X0 = np.zeros_like(TIME)
-    X0[-10+OFFSET1:21+OFFSET1] = 1
-    X0[50+OFFSET1:81+OFFSET1] = 1
+    TIME = np.arange(-50, 101)
+    X0 = TIME * 0.1
     plt.subplot(331)
     plt.plot(TIME, X0)
 
-    SHORTSIGMA, SHORTLENGTH = 0.5, 4
-    LONGSIGMA, LONGLENGTH = 0.2, 10
+    # Filters for short and long term feature
+    SHORTSIGMA, SHORTLENGTH = 0.8, 4
+    LONGSIGMA, LONGLENGTH = 0.4, 10
 
-    # Case 1 : low noise
-    An = 0.2
+
+    # Case 1: Low noise
+    An = 2
     Noise = (np.random.rand(*X0.shape) - 0.5) * An
     X1 = X0 + Noise
     plt.subplot(334)
     plt.plot(TIME, X1)
 
-    Y1S = Filter(X1, SHORTSIGMA, SHORTLENGTH)
+    Y1S = Filter(X1, SHORTSIGMA, SHORTLENGTH, False)
     plt.subplot(335)
     plt.plot(TIME, Y1S)
 
-    Y1L = Filter(X1, LONGSIGMA, LONGLENGTH)
+    Y1L = Filter(X1, LONGSIGMA, LONGLENGTH, False)
     plt.subplot(336)
     plt.plot(TIME, Y1L)
 
-    # Case 2 : high noise
-    An = 0.5
+    # Case 2: High noise
+    An = 10
     Noise = (np.random.rand(*X0.shape) - 0.5) * An
     X2 = X0 + Noise
     plt.subplot(337)
     plt.plot(TIME, X2)
 
-    Y2S = Filter(X2, SHORTSIGMA, SHORTLENGTH)
+    Y2S = Filter(X2, SHORTSIGMA, SHORTLENGTH, False)
     plt.subplot(338)
     plt.plot(TIME, Y2S)
 
-    Y2L = Filter(X2, LONGSIGMA,LONGLENGTH)
+    Y2L = Filter(X2, LONGSIGMA, LONGLENGTH, False)
     plt.subplot(339)
     plt.plot(TIME, Y2L)
 
